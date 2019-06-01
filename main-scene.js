e9ac3c7c0266fe37bbd702fc8abd5892605d0cd5
Young10086 <yourong@ucla.edu>
2019-05-31 19:25:58 -0700

    car

--- main-scene.js
+++ main-scene.js
@@ -14,6 +14,99 @@ const { Square,Cube, Subdivision_Sphere, Transforms_Sandbox_Base, Windmill, Clos
 
 // (Can define Main_Scene's class here)
 
+export class Shape_From_File extends Shape
+{                                   // **Shape_From_File** is a versatile standalone Shape that imports
+                                    // all its arrays' data from an .obj 3D model file.
+  constructor( filename )
+    { super( "position", "normal", "texture_coord" );
+                                    // Begin downloading the mesh. Once that completes, return
+                                    // control to our parse_into_mesh function.
+      this.load_file( filename );
+    }
+  load_file( filename )
+      {                             // Request the external file and wait for it to load.
+                                    // Failure mode:  Loads an empty shape.
+        return fetch( filename )
+          .then( response =>
+            { if ( response.ok )  return Promise.resolve( response.text() )
+              else                return Promise.reject ( response.status )
+            })
+          .then( obj_file_contents => this.parse_into_mesh( obj_file_contents ) )
+          .catch( error => { this.copy_onto_graphics_card( this.gl ); } )
+      }
+  parse_into_mesh( data )
+    {                           // Adapted from the "webgl-obj-loader.js" library found online:
+      var verts = [], vertNormals = [], textures = [], unpacked = {};   
+
+      unpacked.verts = [];        unpacked.norms = [];    unpacked.textures = [];
+      unpacked.hashindices = {};  unpacked.indices = [];  unpacked.index = 0;
+
+      var lines = data.split('\n');
+
+      var VERTEX_RE = /^v\s/;    var NORMAL_RE = /^vn\s/;    var TEXTURE_RE = /^vt\s/;
+      var FACE_RE = /^f\s/;      var WHITESPACE_RE = /\s+/;
+
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        var elements = line.split(WHITESPACE_RE);
+        elements.shift();
+
+        if      (VERTEX_RE.test(line))   verts.push.apply(verts, elements);
+        else if (NORMAL_RE.test(line))   vertNormals.push.apply(vertNormals, elements);
+        else if (TEXTURE_RE.test(line))  textures.push.apply(textures, elements);
+        else if (FACE_RE.test(line)) {
+          var quad = false;
+          for (var j = 0, eleLen = elements.length; j < eleLen; j++)
+          {
+              if(j === 3 && !quad) {  j = 2;  quad = true;  }
+              if(elements[j] in unpacked.hashindices) 
+                  unpacked.indices.push(unpacked.hashindices[elements[j]]);
+              else
+              {
+                  var vertex = elements[ j ].split( '/' );
+
+                  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);   
+                  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+                  
+                  if (textures.length) 
+                    {   unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 0]);
+                        unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 1]);  }
+                  
+                  unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 0]);
+                  unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 1]);
+                  unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 2]);
+                  
+                  unpacked.hashindices[elements[j]] = unpacked.index;
+                  unpacked.indices.push(unpacked.index);
+                  unpacked.index += 1;
+              }
+              if(j === 3 && quad)   unpacked.indices.push( unpacked.hashindices[elements[0]]);
+          }
+        }
+      }
+      {
+      const { verts, norms, textures } = unpacked;
+        for( var j = 0; j < verts.length/3; j++ )
+        { 
+          this.arrays.position     .push( Vec.of( verts[ 3*j ], verts[ 3*j + 1 ], verts[ 3*j + 2 ] ) );        
+          this.arrays.normal       .push( Vec.of( norms[ 3*j ], norms[ 3*j + 1 ], norms[ 3*j + 2 ] ) );
+          this.arrays.texture_coord.push( Vec.of( textures[ 2*j ], textures[ 2*j + 1 ]  ));
+        }
+        this.indices = unpacked.indices;
+      }
+      this.normalize_positions( false );
+      this.ready = true;
+    }
+  draw( context, program_state, model_transform, material )
+    {               // draw(): Same as always for shapes, but cancel all 
+                    // attempts to draw the shape before it loads:
+      if( this.ready )
+        super.draw( context, program_state, model_transform, material );
+    }
+}
+
+
 const Main_Scene =
 class Solar_System extends Scene
 {                                             // **Solar_System**:  Your Assingment's Scene.
@@ -44,10 +137,10 @@ class Solar_System extends Scene
                      'bullet': new defs.Surface_Of_Revolution( 9, 9, points ),
                      'houseup' : new defs.Cone_Tip (4, 4,  [[0,1],[0,1]] ),
                        'housewall' : new defs.Cylindrical_Tube  ( 1, 4,  [[0,2],[0,1]] ),
-                     "teapot": new Solar_System( "assets/dpv.obj" ),
+                     'teapot': new Shape_From_File( "assets/dpv.obj" ),
                       };
 
-
+//
 
 
       //this.shapes = { bullet: new defs.Surface_Of_Revolution( 9, 9, points ) };
@@ -124,8 +217,14 @@ class Solar_System extends Scene
                       eyes: new Material(phong_shader, { ambient: 1, diffusivity: 1, specularity: 0, color: Color.of( 0,0,0.2,1 ) }),
                       fire: new Material( sun_shader, { ambient: 1, color: Color.of( 1,0,0,1 ) } ),
                       grass: new Material(texture_shader_2, { texture: new Texture( "assets/grass.jpg" ),
-ambient: 1, diffusivity: 1, specularity: 0, color: Color.of( 0,0,0,1 ) } )
+                            ambient: 1, diffusivity: 1, specularity: 0, color: Color.of( 0,0,0,1 ) } ),
+              car_texture: new Material(texture_shader_2, { texture: new Texture( "assets/Tex_0020_1.png" ),
+              ambient: 1, diffusivity: 1, specularity: 0, color: Color.of( 0,0,0,1 ) } )
                        };
+/*
+              car_bump: new Material( new defs.Fake_Bump_Map( 1 ), { color: Color.of( 0,0,0,1 ), 
+          ambient: .3, diffusivity: .5, specularity: .5, texture: new Texture( "assets/Tex_0020_6.png" ) });
+          */
 
                                   // Some setup code that tracks whether the "lights are on" (the stars), and also
                                   // stores 30 random location matrices for drawing stars behind the solar system:
@@ -247,9 +346,9 @@ ambient: 1, diffusivity: 1, specularity: 0, color: Color.of( 0,0,0,1 ) } )
    // this.shapes.bullet.draw( context, program_state, model_transform, this.solid.override(yellow) );
   this.shapes.houseup.draw( context, program_state, model_transform.times(Mat4.scale([40,40,10])).times(Mat4.translation([0,0,3])), this.materials. HouU);//.override(Color.of(1,0,0,1)) );
   this.shapes.housewall.draw( context, program_state, model_transform.times(Mat4.scale([40,40,40])), this.solid);
-
+  
 this.shapes.square.draw( context, program_state, Mat4.translation([ 0,-20,-20  ])
-                                       .times( Mat4.rotation( Math.PI, Vec.of( 1,0,0 ) ) ).times( Mat4.scale([ 100,100,1 ]) ),
+                                       .times( Mat4.rotation( Math.PI, Vec.of( 1,0,0 ) ) ).times( Mat4.scale([ 1000,1000,1 ]) ),
                                this.materials.grass);
 
   if (t < 100)
@@ -257,9 +356,13 @@ this.shapes.square.draw( context, program_state, Mat4.translation([ 0,-20,-20  ]
     model_transform = model_transform.times(Mat4.scale([5, 5, 5])).times(Mat4.translation([0,-t/10,0]));
   }
 
-  
-
-                             
+    
+     let car_model = Mat4.identity();
+     //car                               
+this.shapes.teapot.draw( context, program_state, car_model.times(Mat4.scale([22, 22, 22])).times(Mat4.rotation(Math.PI/2, Vec.of( 1,0,0 )).times(Mat4.translation([4,0,0]))),this.materials.car_texture); 
+    
+     
+    // this.shapes.teapot.draw( context, program_state, model_transform,this.materials.plastic_stars);                        
        
      this.shapes.donut.draw(context, program_state, model_transform.times(Mat4.translation([0,0,-0.1])), this.materials.sun.override(Color.of(0.5, 0.5, 0.8, 1)));
      this.shapes.donut.draw(context, program_state, model_transform.times(Mat4.translation([0,0,0.2])), this.materials.sun.override(Color.of(0.5, 0.5, 0.8, 1)));     
